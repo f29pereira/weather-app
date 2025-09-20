06c6900a705d756905ed951095ec5ef649bd1bcf
Filipe Pereira <f29.pereira@gmail.com>
2025-09-20 20:12:48 +0100

    Configure Next.js for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Static export for GitHub Pages
+  basePath: "/weather-app", //GitHub Pages subpath
+  assetPrefix: "/weather-app/", // Prefix assets with repo path
+  images: {
+    unoptimized: true, // Disable Next.js image optimization
+  },
 };
 
 export default nextConfig;
