6c7e02f5197cd13e957a883ee27b87f2dc60d2ff
Filipe Pereira <f29.pereira@gmail.com>
2025-10-17 13:54:23 +0100

    Update fetchLocation, getWeatherImagePath functions and image paths
    - Update fetchLocation to get the six days of the week if the results from the API exist
    - Update getWeatherImagePath image paths

--- src/app/components/PopUp/Days/Days.tsx
+++ src/app/components/PopUp/Days/Days.tsx
@@ -60,7 +60,7 @@ export default function Days() {
         ) ?? null}
         {/*Dropdown icon*/}
         <Image
-          src="images/icons/icon-dropdown.svg"
+          src="/images/icons/icon-dropdown.svg"
           width={16}
           height={16}
           alt=""
--- src/app/utils/weather.ts
+++ src/app/utils/weather.ts
@@ -83,9 +83,8 @@ export const fetchLocation = async (location: string): Promise<Location> => {
     locationData.longitude = result.longitude;
     locationData.description = result.name + ", " + result.country;
     locationData.date = getDate();
+    locationData.days = getSixDaysOfWeek();
   }
-
-  locationData.days = getSixDaysOfWeek();
 
   return locationData;
 };
@@ -117,7 +116,7 @@ export const getWeather = async (
 /**
  * Fetches weather data from Open-Meteo API
  */
-const fetchWeatherData = async (
+export const fetchWeatherData = async (
   latitude: string,
   longitude: string,
   unit: "metric" | "imperial"
@@ -274,31 +273,31 @@ export const getLoadingHourlyForecast = (): HourForecastProps[] => {
 /**
  * Returns the weather image path based of the weatherCode from Open-Meteo API
  */
-const getWeatherImagePath = (weatherCode: number): string => {
+export const getWeatherImagePath = (weatherCode: number): string => {
   switch (weatherCode) {
     //clear sky
     case 0:
-      return "images/icons/icon-sunny.webp";
+      return "/images/icons/icon-sunny.webp";
 
     //partly cloudy
     case 1:
     case 2:
-      return "images/icons/icon-partly-cloudy.webp";
+      return "/images/icons/icon-partly-cloudy.webp";
 
     //overcast
     case 3:
-      return "images/icons/icon-overcast.webp";
+      return "/images/icons/icon-overcast.webp";
 
     //fog
     case 45:
     case 46:
-      return "images/icons/icon-fog.webp";
+      return "/images/icons/icon-fog.webp";
 
     //drizzle
     case 51:
     case 53:
     case 55:
-      return "images/icons/icon-drizzle.webp";
+      return "/images/icons/icon-drizzle.webp";
 
     //Rain
     case 61:
@@ -307,19 +306,19 @@ const getWeatherImagePath = (weatherCode: number): string => {
     case 80:
     case 81:
     case 82:
-      return "images/icons/icon-rain.webp";
+      return "/images/icons/icon-rain.webp";
 
     //snow fall
     case 71:
     case 73:
     case 75:
-      return "images/icons/icon-snow.webp";
+      return "/images/icons/icon-snow.webp";
 
     //thunderstorm
     case 95:
     case 96:
     case 99:
-      return "images/icons/icon-storm.webp";
+      return "/images/icons/icon-storm.webp";
 
     default:
       return "";
